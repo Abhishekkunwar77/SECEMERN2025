fc2ee02b77631e6e65f1d5faf03c175cbed27236
Abhishek Kunwar <153739805+Abhishekkunwar77@users.noreply.github.com>
2025-01-10 10:40:42 +0530

    Add files via upload

---
+++ looping.js
@@ -0,0 +1,27 @@
+//looping in javascript
+// normal and advanced looping tecnhiques
+//normal looping:
+//for 
+//whiledo 
+//while
+//advanced looping:
+//for each 
+//for in 
+//for
+var a=[10,20,30]
+var b={
+   "count":70,
+   "abs":1
+}
+
+for(let i in a){
+console.log(a[i])
+}
+for(let i in b){
+console.log(i,b[i])
+}
+set = new Set(["hello",1,2])
+for(let i of set){
+    console.log(i)
+}
+console.log(set)
