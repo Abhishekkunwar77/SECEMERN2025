9e9331de6702a5961a78f7416bce9d5b61f54972
Abhishek Kunwar <153739805+Abhishekkunwar77@users.noreply.github.com>
2025-01-10 11:30:09 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,12 @@
+//  4 types of functions 
+//with parameters without return
+//with parameters with return
+//without parameters without return
+//without parameters with return
+ 
+ //1.  //with parameters with return
+function SECE(dept){
+    console.log("The department is", dept)
+    return "Section of the department is A"
+}
+console.log(SECE("CSE"))
