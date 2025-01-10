f5b32dcfda77cf8fbef3eb673994a72559770bac
abhishek <aailabeu@gmail.com>
2025-01-10 16:01:21 +0530

    Adding function.js

--- function.js
+++ function.js
@@ -3,3 +3,10 @@
 //with parameters with return
 //without parameters without return
 //without parameters with return
+ 
+ //1.  //with parameters with return
+function SECE(dept){
+    console.log("The department is", dept)
+    return "Section of the department is A"
+}
+console.log(SECE("CSE"))
